c36845a58ecf3d4d97943ffe8906e83e9475ddcb
Abdullah Mansoor <mansoorabdullah001@gmail.com>
2021-05-29 13:58:15 +0530

    Letters table completed

--- converter.js
+++ converter.js
@@ -972,25 +972,16 @@ function resetArea(elementId) {
 }
 
 function showLettersTable() {
-  var win = window.open(
+  window.open(
+    "letters.html",
     "",
-    "Title",
     "toolbar=no,location=no,directories=no,status=no,menubar=no," +
       "scrollbars=yes,resizable=yes,width=" +
       screen.width +
       ",height=" +
       screen.height +
-      ",top=0,left=" +
-      screen.width
+      ",top=0,left=0"
   );
-
-  win.document.write(
-    '<html><head><title>Letters Table</title><link rel="stylesheet" href="bootstrap.min.css" /></head><body>'
-  );
-  win.document.body.innerHTML = document.getElementById(
-    "letters-table"
-  ).innerHTML;
-  win.document.write("</body></html>");
 }
 
 window.onload = counter_fn;
